1723decf52b57455c6187007edb5f4b6755a50d0
Mike Blair <mlblairnotes@outlook.com>
2023-10-31 01:17:25 -0400

    Added blob controller and result object for azure blob storage management api calls.

---
+++ EcommerceApp/Server/Controllers/BlobController.cs
@@ -0,0 +1,74 @@
+﻿using Azure.Storage.Blobs;
+using EcommerceApp.Shared.DTOs;
+using EcommerceApp.Shared.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EcommerceApp.Server.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class BlobController : ControllerBase
+    {
+        private readonly BlobServiceClient _blobServiceClient;
+
+        public BlobController(BlobServiceClient blobServiceClient)
+        {
+            _blobServiceClient = blobServiceClient;
+        }
+
+        public async Task<BlobUploadResult> UploadToBlob(IFormFile file)
+        {
+            var result = new BlobUploadResult();
+            try
+            {
+                var blobContainerClient = _blobServiceClient.GetBlobContainerClient("products");
+                var blobClient = blobContainerClient.GetBlobClient(file.FileName);
+
+                using (var stream = file.OpenReadStream())
+                {
+                    await blobClient.UploadAsync(stream);
+                }
+
+                result.FileUrl = blobClient.Uri.AbsoluteUri;
+                result.IsUploaded = true;
+            }
+            catch (Exception ex)
+            {
+                result.IsUploaded = false;
+                // Log the exception to your logging framework
+                Console.WriteLine($"Exception: {ex.Message}");
+            }
+            return result;
+        }
+
+        [HttpPost("upload")]
+        public async Task<ActionResult<ServiceResponse<BlobUploadResult>>> Upload(IFormFile file)
+        {
+            var response = new ServiceResponse<BlobUploadResult>();
+            // Upload the file to Azure Blob Storage.
+            BlobUploadResult result = await UploadToBlob(file);
+            if(result != null)
+            {
+                response.Success = true;
+                response.Message = "File uploaded successfully!";
+                response.Data = result;
+                // Return the Azure Blob Storage URL and Success Code.
+                return Ok(response);
+            } else
+            {
+                response.Success = false;
+                response.Message = "File upload failed!";
+            }
+            return BadRequest("An error occurred while uploading the file.");
+        }
+
+    }
+    public class BlobUploadResult
+    {
+        public bool IsUploaded { get; set; }
+        public string FileUrl { get; set; }
+    }
+
+
+}
---
+++ EcommerceApp/Shared/Models/BlobUploadResult.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EcommerceApp.Shared.Models
+{
+    public class BlobUploadResult
+    {
+        public bool IsUploaded { get; set; }
+        public string FileUrl { get; set; }
+    }
+
+}
